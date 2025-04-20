c8e41d216e961ef29e4ea4e23bdf810e2a4c4d0c
Yeison <yencardonaal@unal.edu.co>
2025-04-19 19:15:08 -0500

    Black

--- examples/bare_minimum.py
+++ examples/bare_minimum.py
@@ -1,6 +1,7 @@
 import sys
 
 from PySide6 import QtWidgets
+
 # from PySide2 import QtWidgets
 # from PyQt5 import QtWidgets
 # from PyQt6 import QtWidgets
@@ -12,13 +13,12 @@ app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
 
 # setup stylesheet
-apply_stylesheet(app, theme='dark_teal.xml')
+apply_stylesheet(app, theme="dark_teal.xml")
 
 # run
 window.show()
 
-if hasattr(app, 'exec'):
+if hasattr(app, "exec"):
     app.exec()
 else:
     app.exec_()
-
